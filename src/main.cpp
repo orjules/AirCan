9e58340557d467c6f8098bad012c00d48be300a4
Julius G-B <j.gilkaboetzow@gmail.com>
2021-12-19 17:54:18 +0100

    MPU auf Seriell funktioniert

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,51 @@
 #include <Arduino.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+#define MPU6050_ADDR 0x68
+
+Adafruit_MPU6050 mpu;
+
+void connect_MPU(){
+  while(!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+}
+
+void data_to_serial(sensors_event_t *a, sensors_event_t *g){
+  Serial.print("Acc");
+  Serial.print(a -> acceleration.x);
+  Serial.print(",");
+  Serial.print(a -> acceleration.y);
+  Serial.print(",");
+  Serial.print(a -> acceleration.z);
+  Serial.print(", ");
+  Serial.print("Gyro");
+  Serial.print(g ->gyro.x);
+  Serial.print(",");
+  Serial.print(g -> gyro.y);
+  Serial.print(",");
+  Serial.print(g -> gyro.z);
+  Serial.println("");
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  connect_MPU();
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+  data_to_serial(&a, &g);
+
+  delay(20);
 }
